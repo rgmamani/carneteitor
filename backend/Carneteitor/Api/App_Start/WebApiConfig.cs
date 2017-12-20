095b84067250fb1d12515e19b01bb86e4578d99c
gabriel <rgmamani@gmail.com>
2017-12-19 23:11:54 -0300

    Agrego soporte para log4net.

--- backend/Carneteitor/Api/App_Start/WebApiConfig.cs
+++ backend/Carneteitor/Api/App_Start/WebApiConfig.cs
@@ -8,7 +8,7 @@ namespace Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            log4net.Config.XmlConfigurator.Configure();
             // Web API routes
             config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute(
--- backend/Carneteitor/Api/Helper/ReadDataHelper.cs
+++ backend/Carneteitor/Api/Helper/ReadDataHelper.cs
@@ -1,4 +1,6 @@
 ﻿using Api.Models;
+using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,22 +8,33 @@ namespace Api.Helper
 {
     public static class ReadDataHelper
     {
+        //Declare an instance for log4net
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public static List<AfiliadoModel> Read(string path)
         {
-            var book = new LinqToExcel.ExcelQueryFactory(path);
+            try
+            {
+                var book = new LinqToExcel.ExcelQueryFactory(path);
 
-            var query = (from row in book.Worksheet(ConfigurationHelper.SheetName)
-                         let item = new AfiliadoModel
-                         {
-                             Documento = row[ConfigurationHelper.DocumentColumn].Cast<string>(),
-                             Nombre = row[ConfigurationHelper.NameColumn].Cast<string>(),
-                             Apellido = row[ConfigurationHelper.LastnameColumn].Cast<string>(),
-                             Localidad = row[ConfigurationHelper.StateColumn].Cast<string>(),
-                             Provincia = row[ConfigurationHelper.ProvinceColumn].Cast<string>()
-                         }
-                         select item).ToList();
+                var query = (from row in book.Worksheet(ConfigurationHelper.SheetName)
+                             let item = new AfiliadoModel
+                             {
+                                 Documento = row[ConfigurationHelper.DocumentColumn].Cast<string>(),
+                                 Nombre = row[ConfigurationHelper.NameColumn].Cast<string>(),
+                                 Apellido = row[ConfigurationHelper.LastnameColumn].Cast<string>(),
+                                 Localidad = row[ConfigurationHelper.StateColumn].Cast<string>(),
+                                 Provincia = row[ConfigurationHelper.ProvinceColumn].Cast<string>()
+                             }
+                             select item).ToList();
 
-            return query;
+                return query;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Read xls file", ex);
+                throw;
+            }
         }
     }
 }
